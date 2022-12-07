fe193481d954bec755e63f31bfd4760ff59c973e
HashanEranga <ccmrhe@gmail.com>
2022-12-07 23:36:03 +0530

    APP-created a razor application and bind a service and a model

---
+++ 04-WebApp-creation-using-razor-pages/WebAppBookKeeping/Pages/Book.cshtml.cs
@@ -0,0 +1,37 @@
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebAppBookKeeping.Models;
+using WebAppBookKeeping.Services;
+
+namespace WebAppBookKeeping.Pages
+{
+
+    public class BookModel : PageModel
+    {
+        [BindProperty]
+        public Book NewBook { get; set; } = new();
+        public List<Book> books = new();
+        public void OnGet()
+        {
+            books = BookService.GetAllBooks();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            BookService.AddBook(NewBook);
+            return RedirectToAction("Get");
+        }
+
+        public IActionResult OnPostDelete(int Id)
+        {
+            BookService.DeleteBook(Id);
+            return RedirectToAction("Get");
+        }
+    }
+
+}
