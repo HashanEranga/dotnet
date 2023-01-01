df3691696c2e13b9de10955209b47c2c0646be29
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-01 15:40:48 +0530

    httpGet-working-on-custom-metadata

--- 06-HTTPGetPost-methods-webapp/01-httptGet/httpGet/httpGet/Program.cs
+++ 06-HTTPGetPost-methods-webapp/01-httptGet/httpGet/httpGet/Program.cs
@@ -6,10 +6,10 @@ app.MapGet("/", () => "Hello World!");
 app.Run(async (HttpContext context) =>
 {
     context.Response.Headers["Content-type"] = "text/html";
-    if (context.Request.Headers.ContainsKey("User-Agent"))
+    if (context.Request.Headers.ContainsKey("Auth-Key"))
     {
-        string userAgent = context.Request.Headers["User-Agent"];
-        await context.Response.WriteAsync($"<p>{userAgent}</p>");
+        string authKey = context.Request.Headers["Auth-Key"];
+        await context.Response.WriteAsync($"<p>{authKey}</p>");
     }
 });
 
