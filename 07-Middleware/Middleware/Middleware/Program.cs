46a69462cdf391f2e376187a788112ff70e4358d
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-02 01:29:06 +0530

    middleware-chain-created a chain

--- 07-Middleware/Middleware/Middleware/Program.cs
+++ 07-Middleware/Middleware/Middleware/Program.cs
@@ -9,4 +9,30 @@ app.MapGet("/", () => "Hello World!");
 //     await context.Response.WriteAsync("Introduction to Terminating Middleware");
 // });
 
+// creating a middleware intermediate and start middleware using app.Use()
+
+// middleware 01
+app.Use(async (HttpContext context, RequestDelegate next)=>{
+    await context.Response.WriteAsync("This is middleware 01 starts\n");
+    await next(context);
+    await context.Response.WriteAsync("This is middleware 01 ends\n");
+});
+
+// middleware 02
+app.Use(async (HttpContext context, RequestDelegate next)=>{
+    await context.Response.WriteAsync("This is middleware 02 starts\n");
+    await next(context);
+    await context.Response.WriteAsync("This is middleware 02 ends\n");
+});
+
+// middleware 03
+app.Run(async (HttpContext context)=>{
+    await context.Response.WriteAsync("This is middleware 03 starts\n");
+    await context.Response.WriteAsync("This is middleware 03 ends\n");
+});
+
+// the above example uses middleware chaining
+
+
+
 app.Run();
