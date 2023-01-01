b3af8831f12d9300c44e45cb2686234d5d29f3a7
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-02 01:16:46 +0530

    middleware-chain-clean-prev-builds

--- 07-Middleware/Middleware/Middleware/Program.cs
+++ 07-Middleware/Middleware/Middleware/Program.cs
@@ -4,9 +4,9 @@ var app = builder.Build();
 app.MapGet("/", () => "Hello World!");
 
 // creating a middleware terminating middleware 
-app.Run(async (HttpContext context)=>
-{
-    await context.Response.WriteAsync("Introduction to Terminating Middleware");
-});
+// app.Run(async (HttpContext context)=>
+// {
+//     await context.Response.WriteAsync("Introduction to Terminating Middleware");
+// });
 
 app.Run();
