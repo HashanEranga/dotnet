26b27208f7081964da8738e55410bd7105cd818e
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-02 01:10:29 +0530

    middleware-developed-a-terminating-middleware

--- 07-Middleware/Middleware/Middleware/Program.cs
+++ 07-Middleware/Middleware/Middleware/Program.cs
@@ -3,4 +3,10 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// creating a middleware terminating middleware 
+app.Run(async (HttpContext context)=>
+{
+    await context.Response.WriteAsync("Introduction to Terminating Middleware");
+});
+
 app.Run();
