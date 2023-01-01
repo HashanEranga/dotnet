c646806e4ef3d39c4228865020cca929a283fca7
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-01 14:48:36 +0530

    httpGet-getting-default-metadata

--- 06-HTTPGetPost-methods-webapp/01-httptGet/httpGet/httpGet/Program.cs
+++ 06-HTTPGetPost-methods-webapp/01-httptGet/httpGet/httpGet/Program.cs
@@ -1,6 +1,21 @@
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.Run(async (HttpContext context) =>
+{
+    // set the content type to text/html
+    context.Response.Headers["Content-type"] = "text/html";
+
+    // check for default header metadata
+    if (context.Request.Headers.ContainsKey("User-Agent"))
+    {
+        string userAgent = context.Request.Headers["User-Agent"];
+        await context.Response.WriteAsync($"<p>{userAgent}</p>");
+    }
+});
 
 app.Run();
