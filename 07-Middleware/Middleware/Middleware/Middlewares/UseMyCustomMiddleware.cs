ac40ed7ecf738f68664792e11945ecd5f6a752a4
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-03 01:40:39 +0530

    middleware-class- created a middleware class

---
+++ 07-Middleware/Middleware/Middleware/Middlewares/UseMyCustomMiddleware.cs
@@ -0,0 +1,13 @@
+
+namespace Middleware.Middlewares {
+
+    class UseMyCustomMiddleware : IMiddleware{
+
+        public async Task InvokeAsync(HttpContext context, RequestDelegate next){
+            // before logic
+            await context.Response.WriteAsync("This is the custom middleware class\n");
+            await next(context);
+            // after logic
+        }
+    }
+}
--- 07-Middleware/Middleware/Middleware/Program.cs
+++ 07-Middleware/Middleware/Middleware/Program.cs
@@ -1,4 +1,7 @@
+using Middleware.Middlewares;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddTransient<UseMyCustomMiddleware>();
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
@@ -12,27 +15,32 @@ app.MapGet("/", () => "Hello World!");
 // creating a middleware intermediate and start middleware using app.Use()
 
 // middleware 01
-app.Use(async (HttpContext context, RequestDelegate next)=>{
-    await context.Response.WriteAsync("This is middleware 01 starts\n");
-    await next(context);
-    await context.Response.WriteAsync("This is middleware 01 ends\n");
-});
+// app.Use(async (HttpContext context, RequestDelegate next)=>{
+//     await context.Response.WriteAsync("This is middleware 01 starts\n");
+//     await next(context);
+//     await context.Response.WriteAsync("This is middleware 01 ends\n");
+// });
 
-// middleware 02
-app.Use(async (HttpContext context, RequestDelegate next)=>{
-    await context.Response.WriteAsync("This is middleware 02 starts\n");
-    await next(context);
-    await context.Response.WriteAsync("This is middleware 02 ends\n");
-});
+// // middleware 02
+// app.Use(async (HttpContext context, RequestDelegate next)=>{
+//     await context.Response.WriteAsync("This is middleware 02 starts\n");
+//     await next(context);
+//     await context.Response.WriteAsync("This is middleware 02 ends\n");
+// });
 
-// middleware 03
-app.Run(async (HttpContext context)=>{
-    await context.Response.WriteAsync("This is middleware 03 starts\n");
-    await context.Response.WriteAsync("This is middleware 03 ends\n");
-});
+// // middleware 03
+// app.Run(async (HttpContext context)=>{
+//     await context.Response.WriteAsync("This is middleware 03 starts\n");
+//     await context.Response.WriteAsync("This is middleware 03 ends\n");
+// });
 
 // the above example uses middleware chaining
 
+// creating middleware classes
+app.UseMiddleware<UseMyCustomMiddleware>();
 
+app.Run(async (HttpContext context)=>{
+    await context.Response.WriteAsync("This is the Program class middleware\n");
+});
 
 app.Run();
