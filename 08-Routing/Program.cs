037a72c534f4cee1c9ad6be956f771c536a83f6c
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-05 18:12:30 +0530

    Routing - Created POST GET requests

--- 08-Routing/Program.cs
+++ 08-Routing/Program.cs
@@ -6,7 +6,36 @@ app.UseRouting();
 
 // the endpoints are defined inside this method and map them in endpoints
 app.UseEndpoints(endpoints => {
-    
+    // define the default path
+    endpoints.Map("/", async (context) => {
+        await context.Response.WriteAsync("In the default path\n");
+    });
+
+    // define the first endpoint
+    // work with any http method
+    // endpoints.Map("/map1", async (context) => {
+    //     await context.Response.WriteAsync("In Map 1\n");
+    // });
+
+    // allow only http specific http methods
+    endpoints.MapGet("/map1", async (context) => {
+        await context.Response.WriteAsync("In Map1 using a get method only")
+    });
+
+    // define the second endpoint
+    // endpoints.Map("/map2", async (context) => {
+    //     await context.Response.WriteAsync("In Map 2\n");
+    // });
+
+    // this endpoint will allow only the post methods only
+    endpoints.MapPost("/map2", async (context) => {
+        await context.Response.WriteAsync("In Map2 using a post method only");
+    });
+});
+
+// configure for other paths were added
+app.Run(async context => {
+    await context.Response.WriteAsync($"Request path is {context.Request.Path}");
 });
 
 app.Run();
