2892f0463ec95631651f7d51b94707fcaf616d08
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-05 20:11:03 +0530

    Routing - Created router parameters

--- 08-Routing/Program.cs
+++ 08-Routing/Program.cs
@@ -5,31 +5,40 @@ var app = builder.Build();
 app.UseRouting();
 
 // the endpoints are defined inside this method and map them in endpoints
-app.UseEndpoints(endpoints => {
-    // define the default path
-    endpoints.Map("/", async (context) => {
-        await context.Response.WriteAsync("In the default path\n");
-    });
+// app.UseEndpoints(endpoints => {
+//     // define the default path
+//     endpoints.Map("/", async (context) => {
+//         await context.Response.WriteAsync("In the default path\n");
+//     });
 
-    // define the first endpoint
-    // work with any http method
-    // endpoints.Map("/map1", async (context) => {
-    //     await context.Response.WriteAsync("In Map 1\n");
-    // });
+//     // define the first endpoint
+//     // work with any http method
+//     // endpoints.Map("/map1", async (context) => {
+//     //     await context.Response.WriteAsync("In Map 1\n");
+//     // });
 
-    // allow only http specific http methods
-    endpoints.MapGet("/map1", async (context) => {
-        await context.Response.WriteAsync("In Map1 using a get method only")
-    });
+//     // allow only http specific http methods
+//     endpoints.MapGet("/map1", async (context) => {
+//         await context.Response.WriteAsync("In Map1 using a get method only")
+//     });
 
-    // define the second endpoint
-    // endpoints.Map("/map2", async (context) => {
-    //     await context.Response.WriteAsync("In Map 2\n");
-    // });
+//     // define the second endpoint
+//     // endpoints.Map("/map2", async (context) => {
+//     //     await context.Response.WriteAsync("In Map 2\n");
+//     // });
 
-    // this endpoint will allow only the post methods only
-    endpoints.MapPost("/map2", async (context) => {
-        await context.Response.WriteAsync("In Map2 using a post method only");
+//     // this endpoint will allow only the post methods only
+//     endpoints.MapPost("/map2", async (context) => {
+//         await context.Response.WriteAsync("In Map2 using a post method only");
+//     });
+// });
+
+// accessing parameters
+app.UseEndpoints( endpoints => {
+    endpoints.MapGet("/files/{filename}.{extension}", async context => {
+        string? fileName = context.Request.RouteValues["filename"].ToString();
+        string? extension = context.Request.RouteValues["extension"].ToString();
+        await context.Response.WriteAsync($"In files accessed {fileName}.{extension}");
     });
 });
 
