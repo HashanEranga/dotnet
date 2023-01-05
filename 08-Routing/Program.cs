78590c145466dc9af9a167eaa197a46a44673ecf
HashanEPerera <45037796+HashanEranga@users.noreply.github.com>
2023-01-05 17:16:01 +0530

    Routing - Enabled Routing

--- 08-Routing/Program.cs
+++ 08-Routing/Program.cs
@@ -1,6 +1,12 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// use Routing for enabling routing
+app.UseRouting();
+
+// the endpoints are defined inside this method and map them in endpoints
+app.UseEndpoints(endpoints => {
+    
+});
 
 app.Run();
